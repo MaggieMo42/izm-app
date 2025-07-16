40790a3fa6a66c5ff6fe6138dae175ad40adf10a
MaggieMo42 <you@example.com>
2025-07-16 12:24:39 +0200

    dio

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import Lokacija from '../parts/lokacija.jpg';
 
 const Contact = () => {
   return (
-    <div className="contact-container">
+    <div className="contact-section">
+      <div className="contact-info">
+        <p>Kako do nas?</p>
+      </div>    
+<div className="container">
+  <div className="row">
+    {/* First column for iframe */}
+    <div className="col-md-6">
       <iframe
+        style={{ width: '100%', height: '450px', border: 0 }}
         title="Google Map - Studio Zen Place"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2858.067155231049!2d13.848275576274453!3d44.850026971070506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x67d110a47891ba33%3A0x39e74d17769202d4!2sStudio%20Zen%20place%20~%20holistic%20studio%20for%20yoga%2C%20sound%20massage%20and%20wellbeing!5e1!3m2!1sen!2shr!4v1752626452825!5m2!1sen!2shr"
-        width="100%"
-        height="450"
-        style={{ border: 0 }}
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
-      
     </div>
+    
+    {/* Second column for image */}
+    <div className="col-md-6">
+      <img 
+        src={Lokacija} 
+        alt="Lokacija studija Zen place Pula" 
+        style={{ width: '100%', height: 'auto' }} 
+      />
+    </div>
+  </div>
+</div>
+</div>
   );
 };
-
-export default Contact
+export default Contact;
--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -6,17 +6,14 @@ const Footer = () => {
     <footer className="bg-light text-dark pt-4">
       <div className="container">
         <div className="row">
-        
           <div className="col-md-4 col-lg-4 col-xl-4 mb-4">
             <h6 className="text-uppercase fw-bold">Usluge</h6>
             <hr className="mb-4 mt-0 d-inline-block mx-auto" />
-<p><Link to="/ZvucnaMasaza" className="text-dark">Zvučna masaža</Link></p>
-<p><Link to="/KundaliniYoga" className="text-dark">Kundalini yoga</Link></p>
-<p><Link to="/ThetaHealing" className="text-dark">Theta healing</Link></p>
-<p><Link to="/HumanDesign" className="text-dark">Human Design</Link></p>
+            <p><Link to="/ZvucnaMasaza" className="text-dark">Zvučna masaža</Link></p>
+            <p><Link to="/KundaliniYoga" className="text-dark">Kundalini yoga</Link></p>
+            <p><Link to="/ThetaHealing" className="text-dark">Theta healing</Link></p>
+            <p><Link to="/HumanDesign" className="text-dark">Human Design</Link></p>
           </div>
-
-          
           <div className="col-md-4 col-lg-4 col-xl-4 mb-4">
             <h6 className="text-uppercase fw-bold">Kontakt</h6>
             <hr className="mb-4 mt-0 d-inline-block mx-auto" />
